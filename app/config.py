b7147c8f771a923b53da4a63c28f5dc6e2dd451c
Johan Méndez <johanmendezh@habi.co>
2024-11-18 17:06:57 -0500

    [UPDATE] Symbol configuration

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
 BINANCE_SECRET_KEY = os.getenv("BINANCE_SECRET_KEY")
 
 investment = 100
-CRYPTO = [
+SYMBOLS = [
     "BTCUSDT",
     "ETHUSDT",
     "BNBUSDT"
