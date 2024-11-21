98fba65cb1f36d2f06f01e1edab538d26f216356
Johan Méndez <johanmendezh@habi.co>
2024-11-21 12:52:01 -0500

    [ADD] download logic to fetch data

--- app/services/binance_service.py
+++ app/services/binance_service.py
@@ -1,10 +1,11 @@
 import os
 import time
-
-from binance.client import Client
+import pandas as pd
+from datetime import datetime, timedelta
 from requests.exceptions import HTTPError
 
-import pandas as pd
+from binance.client import Client
+from app.services.risk_analysis import RiskAnalysis
 from app.services.logger_service import LoggerService
 
 from IPython import embed
@@ -16,16 +17,37 @@ class BinanceService:
         self.client = Client(api_key, secret_key)
         self.output_dir = output_dir
         self.logger = LoggerService()
+        self.risk_analyzer = RiskAnalysis()
+
+    def _is_data_stale(self, symbol, interval="1h"):
+        file_path = os.path.join(self.output_dir, f"{symbol}_{interval}.csv")
+        if not os.path.exists(file_path):
+            self.logger.log("INFO", f"No data file_found for {symbol}.")
+            return True
+        
+        df = pd.read_csv(file_path)
+        if df.empty:
+            self.logger.log("INFO", f"Data file for {symbol} is empty.")
+            return True
+
+        last_record_time = pd.to_datetime(df["time"].iloc[-1])
+        time_diference = datetime.utcnow() - last_record_time
+        self.logger.log("INFO", f"Last record time for {symbol}: {last_record_time}")
+        return time_diference > timedelta(hours=1)
 
 
     def fetch_historical_data(self, symbol, interval="1h", start_date="2017-01-01"):
-        all_data = []
-        start_timestamp = int(pd.to_datetime(start_date).timestamp() * 1000)
         max_attemps = 5
-
+        
+        if not self._is_data_stale(symbol, interval):
+            self.logger.log("INFO", f"Data for {symbol} is up-to-date.")
+            return pd.read_csv(os.path.join(self.output_dir, f"{symbol}_{interval}.csv"))
+        
         self.logger.log("INFO", f"Starting data fetch for {symbol} with interval {interval} from {start_date}.")
 
-    
+        all_data = []
+        start_timestamp = int(pd.to_datetime(start_date).timestamp() * 1000)
+
         while True:
             try:
                 klines = self.client.get_klines(
@@ -34,9 +56,11 @@ class BinanceService:
                     startTime=start_timestamp,
                     limit=1000
                 )
+
                 if not klines:
                     self.logger.log("INFO", f"No more data available for {symbol}.")
                     break
+
                 for kline in klines:
                     all_data.append({
                         "time": pd.to_datetime(kline[0], unit="ms"),  # Timestamp en milisegundos
@@ -63,6 +87,8 @@ class BinanceService:
                 time.sleep(1)
 
         df = pd.DataFrame(all_data)
+        df.to_csv(os.path.join(self.output_dir, f"{symbol}_{interval}.csv"), index=False)
+        self.logger.log("INFO", f"Data saved for {symbol}. Total rows: {len(df)}")
         return df
 
     def test_connection(self):
